b7e8a173e39e95e28da368e51a64b4fab28642c4
Taylor Baldwin <taylorbaldwin@gmail.com>
2016-07-30 14:37:42 -0400

    use standard for linting

--- src/encode-object.js
+++ src/encode-object.js
@@ -1,115 +1,114 @@
-import hashObject from 'object-hash';
-import leftPad from 'left-pad';
-import { toBase62, fromBase62, toBase2, fromBase2 } from 'bases';
+import hashObject from 'object-hash'
+import leftPad from 'left-pad'
+import { toBase62, fromBase62, toBase2, fromBase2 } from 'bases'
 
+const VERSION_LENGTH = 2
+const alphabetize = (a, b) => (a < b ? -1 : 1)
 
-const VERSION_LENGTH = 2;
-const alphabetize = (a, b) => (a < b ? -1 : 1);
-
-function validateConfig(config) {
+function validateConfig (config) {
   Object.keys(config).forEach((key) => {
-    const [min, max, step] = config[key];
-    if (min > max) throw new RangeError(`min is greater than max for field: "${key}"`);
+    const [min, max, step] = config[key]
+    if (min > max) throw new RangeError(`min is greater than max for field: "${key}"`)
     if ((max - min) % step !== 0) {
       throw new RangeError(
         `the range between min and max must be divisible by step ${step} for field: ${key}`
-      );
+      )
     }
-  });
+  })
 }
 
-export default function createEncoder(config) {
-  if (!config) throw new Error('config required');
+export default function createEncoder (config) {
+  if (!config) throw new Error('config required')
 
-  const configKeys = Object.keys(config);
-  configKeys.sort(alphabetize);
+  const configKeys = Object.keys(config)
+  configKeys.sort(alphabetize)
 
-  let totalBitsNeeded = 0;
+  let totalBitsNeeded = 0
 
   // fill in defaults
-  config = { ...config };
+  config = { ...config }
   configKeys.forEach((key) => {
-    const [min, max, step = 1] = config[key];
-    if (step === 0) throw new RangeError(`step cannot be 0 for field: ${key}`);
-    const steps = ((max - min) / step) + 1; // plus one because range is inclusive
-    const bitsNeeded = Math.ceil(Math.log2(steps));
-    totalBitsNeeded += bitsNeeded;
-    config[key] = [min, max, step, bitsNeeded];
-  });
+    const [min, max, step = 1] = config[key]
+    if (step === 0) throw new RangeError(`step cannot be 0 for field: ${key}`)
+    const steps = ((max - min) / step) + 1 // plus one because range is inclusive
+    const bitsNeeded = Math.ceil(Math.log2(steps))
+    totalBitsNeeded += bitsNeeded
+    config[key] = [min, max, step, bitsNeeded]
+  })
 
-  validateConfig(config);
+  validateConfig(config)
 
-  const configVersion = hashObject(config).slice(0, VERSION_LENGTH);
+  const configVersion = hashObject(config).slice(0, VERSION_LENGTH)
 
-  function validateObject(obj) {
-    const objKeys = Object.keys(obj);
-    const missingConfigKeys = objKeys.filter(key => !configKeys.includes(key));
-    const missingObjKeys = configKeys.filter(key => !objKeys.includes(key));
+  function validateObject (obj) {
+    const objKeys = Object.keys(obj)
+    const missingConfigKeys = objKeys.filter(key => !configKeys.includes(key))
+    const missingObjKeys = configKeys.filter(key => !objKeys.includes(key))
     if (missingConfigKeys.length) {
-      throw new Error(`config is missing fields: ${missingConfigKeys.join(', ')}`);
+      throw new Error(`config is missing fields: ${missingConfigKeys.join(', ')}`)
     }
     if (missingObjKeys.length) {
-      throw new Error(`object is missing config fields: ${missingObjKeys.join(', ')}`);
+      throw new Error(`object is missing config fields: ${missingObjKeys.join(', ')}`)
     }
 
     configKeys.forEach((key) => {
-      const [min, max, step] = config[key];
-      const val = obj[key];
+      const [min, max, step] = config[key]
+      const val = obj[key]
 
       if (!Number.isFinite(val)) {
-        throw new TypeError('all object values must be numbers');
+        throw new TypeError('all object values must be numbers')
       }
 
       if (val > max) {
-        throw new RangeError(`value ${val} is greater than ${max} for field "${key}"`);
+        throw new RangeError(`value ${val} is greater than ${max} for field "${key}"`)
       }
 
       if (val < min) {
-        throw new RangeError(`value ${val} is less than ${min} for field "${key}"`);
+        throw new RangeError(`value ${val} is less than ${min} for field "${key}"`)
       }
 
       if ((val - min) % step !== 0) {
         throw new RangeError(
           `value ${val} cannot be reached from minimum ${min} by step ${step} for field "${key}"`
-        );
+        )
       }
-    });
+    })
   }
 
-  function validateHash(hash) {
-    const version = hash.slice(0, VERSION_LENGTH);
+  function validateHash (hash) {
+    const version = hash.slice(0, VERSION_LENGTH)
     if (version !== configVersion) {
-      throw new Error('hash was encoded with different config version');
+      throw new Error('hash was encoded with different config version')
     }
   }
 
-  function encodeObject(obj) {
-    validateObject(obj);
+  function encodeObject (obj) {
+    validateObject(obj)
     const bits = configKeys.map((key) => {
-      const [min, , step, bitsNeeded] = config[key];
-      const val = obj[key];
+      const [min, , step, bitsNeeded] = config[key]
+      const val = obj[key]
       // NOTE: rounding here because of floating point errors?
-      const valInBase2 = toBase2(Math.round((val - min) / step));
-      return leftPad(valInBase2, bitsNeeded, '0');
-    }).join('');
-    return configVersion + toBase62(fromBase2(bits));
+      const valInBase2 = toBase2(Math.round((val - min) / step))
+      return leftPad(valInBase2, bitsNeeded, '0')
+    }).join('')
+    return configVersion + toBase62(fromBase2(bits))
   }
 
-  function decodeObject(hash) {
-    const obj = {};
-    validateHash(hash);
-    hash = hash.slice(VERSION_LENGTH);
-    let bits = toBase2(fromBase62(hash));
-    bits = leftPad(bits, totalBitsNeeded, '0');
+  function decodeObject (hash) {
+    const obj = {}
+    validateHash(hash)
+    hash = hash.slice(VERSION_LENGTH)
+    let bits = toBase2(fromBase62(hash))
+    bits = leftPad(bits, totalBitsNeeded, '0')
     configKeys.forEach((key) => {
-      const [min, , step, bitsNeeded] = config[key];
-      const bit = bits.slice(0, bitsNeeded);
-      const val = fromBase2(bit);
-      obj[key] = (val * step) + min;
-      bits = bits.slice(bitsNeeded);
-    });
-    return obj;
+      const [min, , step, bitsNeeded] = config[key]
+      const bit = bits.slice(0, bitsNeeded)
+      const val = fromBase2(bit)
+      obj[key] = (val * step) + min
+      bits = bits.slice(bitsNeeded)
+    })
+    return obj
   }
 
-  return { decodeObject, encodeObject };
+  return { decodeObject, encodeObject }
 }
--- test.js
+++ test.js
@@ -1,75 +1,74 @@
-import test from 'tape'; // eslint-disable-line
-import createEncoder from './lib/encode-object';
-
+import test from 'tape'
+import createEncoder from './lib/encode-object'
 
 test('createEncoder should throw if no config is supplied', (t) => {
-  t.throws(() => createEncoder());
-  t.end();
-});
+  t.throws(() => createEncoder())
+  t.end()
+})
 
 test('encodeObject should throw if config includes fields not in obj', (t) => {
   const { encodeObject } = createEncoder({
     a: [0, 500],
-    b: [0, 10],
-  });
-  t.throws(() => encodeObject({ a: 362 }));
-  t.end();
-});
+    b: [0, 10]
+  })
+  t.throws(() => encodeObject({ a: 362 }))
+  t.end()
+})
 
 test('encodeObject should throw if obj includes fields not in config', (t) => {
-  const { encodeObject } = createEncoder({ a: [0, 200] });
+  const { encodeObject } = createEncoder({ a: [0, 200] })
   t.throws(() => encodeObject({
     a: 123,
-    b: 456,
-  }));
-  t.end();
-});
+    b: 456
+  }))
+  t.end()
+})
 
 test('encodeObject should throw for invalid values', (t) => {
-  const { encodeObject } = createEncoder({ a: [0, 10] });
-  t.throws(() => encodeObject({ a: [] }));
-  t.throws(() => encodeObject({ a: {} }));
-  t.throws(() => encodeObject({ a: true }));
-  t.throws(() => encodeObject({ a: Infinity }));
-  t.throws(() => encodeObject({ a: NaN }));
-  t.end();
-});
+  const { encodeObject } = createEncoder({ a: [0, 10] })
+  t.throws(() => encodeObject({ a: [] }))
+  t.throws(() => encodeObject({ a: {} }))
+  t.throws(() => encodeObject({ a: true }))
+  t.throws(() => encodeObject({ a: Infinity }))
+  t.throws(() => encodeObject({ a: NaN }))
+  t.end()
+})
 
 test('encodeObject should throw for values greater than max', (t) => {
-  const { encodeObject } = createEncoder({ a: [100, 200] });
-  t.throws(() => encodeObject({ a: 201 }));
-  t.end();
-});
+  const { encodeObject } = createEncoder({ a: [100, 200] })
+  t.throws(() => encodeObject({ a: 201 }))
+  t.end()
+})
 
 test('encodeObject should throw for values less than min', (t) => {
-  const { encodeObject } = createEncoder({ a: [100, 200] });
-  t.throws(() => encodeObject({ a: 99 }));
-  t.end();
-});
+  const { encodeObject } = createEncoder({ a: [100, 200] })
+  t.throws(() => encodeObject({ a: 99 }))
+  t.end()
+})
 
 test('encodeObject should throw for values unreachable by step', (t) => {
-  const { encodeObject } = createEncoder({ a: [0, 10, 2] });
-  t.throws(() => encodeObject({ a: 3 }));
-  t.end();
-});
+  const { encodeObject } = createEncoder({ a: [0, 10, 2] })
+  t.throws(() => encodeObject({ a: 3 }))
+  t.end()
+})
 
 test('decodeObject should throw for hashes encoded with different config', (t) => {
-  const encoder1 = createEncoder({ a: [0, 10] });
-  const encoder2 = createEncoder({ a: [10, 20] });
-  const encoded = encoder1.encodeObject({ a: 8 });
-  t.throws(() => encoder2.decodeObject(encoded));
-  t.end();
-});
+  const encoder1 = createEncoder({ a: [0, 10] })
+  const encoder2 = createEncoder({ a: [10, 20] })
+  const encoded = encoder1.encodeObject({ a: 8 })
+  t.throws(() => encoder2.decodeObject(encoded))
+  t.end()
+})
 
 test('decodeObject should correctly decode for hashes encoded with equivalent config', (t) => {
-  const encoder1 = createEncoder({ a: [10, 20] });
-  const encoder2 = createEncoder({ a: [10, 20] });
-  const testObj = { a: 15 };
-  const encoded = encoder1.encodeObject(testObj);
-  const decoded = encoder2.decodeObject(encoded);
-  t.deepEqual(testObj, decoded);
-  t.end();
-});
+  const encoder1 = createEncoder({ a: [10, 20] })
+  const encoder2 = createEncoder({ a: [10, 20] })
+  const testObj = { a: 15 }
+  const encoded = encoder1.encodeObject(testObj)
+  const decoded = encoder2.decodeObject(encoded)
+  t.deepEqual(testObj, decoded)
+  t.end()
+})
 
 test('encoder should decode to the same object that was encoded', (t) => {
   const { encodeObject, decodeObject } = createEncoder({
@@ -80,8 +79,8 @@ test('encoder should decode to the same object that was encoded', (t) => {
     e: [0, 1, 0.25], // floats not officially supported
     f: [-100, 100, 0.5], // floats not officially supported
     g: [0.5, 1, 0.25], // floats not officially supported
-    h: [0, 123456, 24],
-  });
+    h: [0, 123456, 24]
+  })
   const testObj = {
     a: 55,
     b: 12,
@@ -90,10 +89,10 @@ test('encoder should decode to the same object that was encoded', (t) => {
     e: 0.25, // floats not officially supported
     f: -23.5, // floats not officially supported
     g: 0.75, // floats not officially supported
-    h: 696,
-  };
-  const encoded = encodeObject(testObj);
-  const decoded = decodeObject(encoded);
-  t.deepEqual(testObj, decoded);
-  t.end();
-});
+    h: 696
+  }
+  const encoded = encodeObject(testObj)
+  const decoded = decodeObject(encoded)
+  t.deepEqual(testObj, decoded)
+  t.end()
+})
